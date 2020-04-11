5414465d6f22bcf5298818b591af4043242ec86a
Fran Villalta <francisco.villalta@buscoextra.es>
2020-04-11 22:19:57 +0200

    Updates

---
+++ precache-manifest.607a1afc2a1ee8a914bc0f8d50608fc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "850ff1ccd1b6069e269a5f9f9cfdba91",
+    "url": "/franvillaltasilva.github.io/index.html"
+  },
+  {
+    "revision": "7e7a7296f61980bcfca8",
+    "url": "/franvillaltasilva.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7f141faa93254d62ba5a",
+    "url": "/franvillaltasilva.github.io/static/js/2.d90ae76b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/franvillaltasilva.github.io/static/js/2.d90ae76b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e7a7296f61980bcfca8",
+    "url": "/franvillaltasilva.github.io/static/js/main.e65f6ab1.chunk.js"
+  },
+  {
+    "revision": "9029b45ab5482872abe9",
+    "url": "/franvillaltasilva.github.io/static/js/runtime-main.2f0b9980.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/franvillaltasilva.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
