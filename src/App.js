892c73db45fdc26134dc71e410417291541a885e
Fran Villalta <francisco.villalta@buscoextra.es>
2020-04-12 00:18:26 +0200

    working in progress and pug

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import Animation from './components/Animation';
+import workInProgress from './lotties/work-in-progress.json'
+import dog from './lotties/dog.json'
 import './App.css';
 
 function App() {
+
+  const dogWidth = 120;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Animation src={workInProgress}/>
+        <Animation src={dog} width={dogWidth}/>
+
       </header>
     </div>
   );
---
+++ src/components/Animation.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import Lottie from 'react-lottie'
+import workInProgress from '../lotties/work-in-progress.json'
+
+class Animation extends Component {
+
+
+  render(){
+
+    const defaultOptions = {
+      loop: true,
+      autoplay: true,
+      animationData: this.props.src,
+      rendererSettings: {
+        preserveAspectRatio: 'xMidYMid slice'
+      }
+    };
+
+    return(
+      <div>
+        <Lottie options={defaultOptions}
+              height={this.props.height}
+              width={this.props.width}
+        />
+      </div>
+    )
+  }
+}
+
+export default Animation;
